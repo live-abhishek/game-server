fba4c35762dbadd58b1b007b14e0f3b6e7b8f7f0
Abhisheks Simla <abhishek.130490@gmail.com>
2017-06-29 00:00:34 +0530

    -Added basic spring boot app

---
+++ src/main/java/live/abhishek/gameserver/GameserverApplication.java
@@ -0,0 +1,12 @@
+package live.abhishek.gameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameserverApplication.class, args);
+	}
+}

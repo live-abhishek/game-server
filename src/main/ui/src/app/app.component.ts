26983d90fd1fdde8235e89a664c15871eea36b19
Abhisheks Simla <abhishek.130490@gmail.com>
2017-07-11 09:39:25 +0530

    Added test prime ng component

--- src/main/ui/src/app/app.component.ts
+++ src/main/ui/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ConfirmationService } from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ConfirmationService]
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private confirmationService: ConfirmationService) { }
+
+  confirm() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to perform this action?',
+      accept: () => {
+        //Actual logic to perform a confirmation
+      }
+    });
+  }
 }
--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ButtonModule } from 'primeng/primeng';
+import { ConfirmDialogModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 
@@ -11,8 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ConfirmDialogModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

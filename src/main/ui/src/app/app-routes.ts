626a9378163d7b0c915d09c6e4cc9f8605cd894f
Abhisheks Simla <abhishek.130490@gmail.com>
2017-07-16 15:37:55 +0530

    -Added dashboard component -Added basic routes page to dashboard

--- src/main/ui/src/app/app-routes.ts
+++ src/main/ui/src/app/app-routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const appRoutes: Routes = [
-
+    {
+        path: "",
+        component: DashboardComponent
+    }
 ]

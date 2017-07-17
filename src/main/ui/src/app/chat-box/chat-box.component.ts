8437c2293e8e9ae08a1938fb728df38fe7328d84
Abhisheks Simla <abhishek.130490@gmail.com>
2017-07-17 09:37:33 +0530

    Added ability to send and receive chat messages

--- src/main/ui/src/app/chat-box/chat-box.component.ts
+++ src/main/ui/src/app/chat-box/chat-box.component.ts
@@ -1,23 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ChatMessage } from '../messages/message.model';
+
+import { StompService } from 'ng2-stomp-service';
 
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.css']
 })
-export class ChatBoxComponent implements OnInit {
+export class ChatBoxComponent implements OnInit, OnDestroy {
 
   private messages: ChatMessage[] = [];
+  private currentChatText: String;
 
-  constructor() { }
+  private subscribed: boolean;
+  private subscription: any;
+
+  constructor(private stompService: StompService) { }
 
   ngOnInit() {
-    let message1 = new ChatMessage();
-    message1.sender = "Masterchief"; message1.text = "We are just getting started!";
-    let message2 = new ChatMessage();
-    message2.sender = "Cortana"; message2.text = "Don't make a girl a promise if you know you can't keep it";
-    this.messages.push(message1, message2);
+    this.configureStomp();
+    this.subscribe();
   }
+
+  configureStomp(): void {
+    this.stompService.configure({
+      host: 'http://192.168.0.9:8080/chat',
+      debug: true,
+      queue: { 'init': false },
+    });
+  }
+
+  send(): void {
+    this.stompService.send('/app/send', this.currentChatText);
+    this.currentChatText = null;
+  }
+
+  subscribe() {
+    if (this.subscribed) {
+      return;
+    }
+
+    this.stompService.startConnect().then(() => {
+      this.stompService.done('init');
+      console.log('connected with WS');
+      this.subscription = this.stompService.subscribe('/topic/messages', this.on_next);
+      this.subscribed = true;
+    });
+  }
+
+  on_next = (res: any) => {
+    this.messages.push(res);
+    console.log(JSON.stringify(res));
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe();
+  }
+
+  unsubscribe() {
+    if (!this.subscribed) {
+      return;
+    }
+
+    // This will internally unsubscribe from Stomp Broker
+    // There are two subscriptions - one created explicitly, the other created in the template by use of 'async'
+    this.subscription.unsubscribe();
+    this.subscription = null;
+    this.subscribed = false;
+  }
+
 
 }
